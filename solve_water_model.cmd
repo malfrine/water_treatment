ec8741b7368a59862f93422ccc887a9f2f59185e
malfrine <malfy.das@gmail.com>
2017-09-15 15:30:28 -0600

    updated model obj:cost

--- solve_water_model.cmd
+++ solve_water_model.cmd
@@ -1,11 +1,12 @@
+ECHO OFF
+
 cls
 del /f arrows.tex
 del /f model_BFD.pdf
 
-gams minlp_main.gms
+rem gams minlp_main.gms
 matlab -nodisplay -nosplash -nodesktop -r "run('main_script.m'); exit;"
 TIMEOUT /T 20 >nul
 pdflatex model_BFD.tex
 TIMEOUT /T 5 >nul
 start model_BFD.pdf
-
